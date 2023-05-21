b0564c7f29161574bb78397dd3aa36586cb4db9f
macave <marcuscave0112@gmail.com>
2023-05-21 14:17:01 +1000

    added manage additional units

--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
 let filter = "available";
+let extraMICA = 5;
 
 const RENDERERS = {
   status: (unit, row) => {
@@ -20,7 +21,7 @@ const RENDERERS = {
   }
 };
 
-function resetFilter() {  
+function resetFilter() {
   document.getElementById("all").checked = false;
   document.getElementById("available").checked = true;
   //Only allow one dispatch vallidation
@@ -41,7 +42,7 @@ function render() {
   document.querySelector("#randomGrid").textContent = window.grid.grid;
   document.querySelector("#randomEventType").textContent = window.grid.evtype;
   document.querySelector("#randomEventDescriptor").textContent = window.grid.evdescriptor;
-
+  extraMICA = 0;
   const tbody = document.querySelector("#unit-table-body");
 
   tbody.innerHTML = "";
@@ -60,7 +61,83 @@ function render() {
   limited_units.map((unit) => {
     const row = document.createElement("tr");
     Object.keys(unit)
-      .filter((key) => key !== "d_group") 
+      .filter((key) => key !== "d_group")
+      .forEach((key) => {
+        const cell = document.createElement("td");
+
+        if (RENDERERS[key]) {
+          RENDERERS[key](unit, row);
+        } else {
+          let cell = document.createElement("td");
+          cell.textContent = unit[key];
+          row.appendChild(cell);
+        }
+      });
+    tbody.appendChild(row);
+  });
+}
+
+/*Add ALL units
+function addAllUnits() {
+  const tbody = document.querySelector("#unit-table-body");
+  tbody.innerHTML = "";
+
+  let units = [...window.units];
+  if (filter == "available") {
+    units = units.filter((unit) => {
+      return ["AS", "AM"].includes(unit.status);
+    });
+  }
+
+  const limited_units = units.splice(0, 14);
+  limited_units.push(units.find((unit) => unit.unit_type === "MICA"));
+
+  limited_units.map((unit) => {
+    const row = document.createElement("tr");
+    Object.keys(unit)
+      .filter((key) => key !== "d_group")
+      .forEach((key) => {
+        const cell = document.createElement("td");
+
+        if (RENDERERS[key]) {
+          RENDERERS[key](unit, row);
+        } else {
+          let cell = document.createElement("td");
+          cell.textContent = unit[key];
+          row.appendChild(cell);
+        }
+      });
+    tbody.appendChild(row);
+  });
+}*/
+
+//Add MICA Units
+function addMICAUnits() {
+
+  const tbody = document.querySelector("#unit-table-body");
+  extraMICA = extraMICA + 5;
+
+  let units = [...window.units];
+
+
+  if (filter == "available") {
+    units = units.filter((unit) => {
+      return ["AS", "AM"].includes(unit.status);
+    });
+  }
+
+  units.splice(0, 15);
+
+  units = units.filter((unit) => {
+    return ["MICA"].includes(unit.unit_type);
+  });
+  units.splice(0, extraMICA);
+  const limited_units = units.splice(0, 5);
+
+  limited_units.map((unit) => {
+    const row = document.createElement("tr");
+    Object.keys(unit)
+      .filter((key) => key !== "d_group")
       .forEach((key) => {
         const cell = document.createElement("td");
 
